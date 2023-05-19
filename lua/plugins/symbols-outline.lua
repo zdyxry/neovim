2a3d87fc3a8e1283a0d7160d0acab6b0250786bb
Yiran Zhou <yiran@smartx.com>
2023-05-19 16:51:21 +0800

    add symbols-outline.nvim

---
+++ lua/plugins/symbols-outline.lua
@@ -0,0 +1,20 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+-- if true then return {} end
+
+-- every spec file under config.plugins will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+
+  -- add symbols-outline
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    config = true,
+  },
+}
